6a9130242d059fdce1c368c805613c16e22a813c
bkb-Git <82142770+bkb-Git@users.noreply.github.com>
2021-06-07 13:34:43 +0300

    Updated the animations

--- src/sns.js
+++ src/sns.js
@@ -3,7 +3,7 @@ import { faAddressBook, faHome } from "@fortawesome/free-solid-svg-icons";
 
 const sns = [
     { id: 1 , name: 'Home',link: '/' , icon: faHome , color: '#008000' },
-    { id: 2 , name: 'About', link: '/about', icon: faAddressBook , color: '#442863'},
+    { id: 2 , name: 'About', link: '/about', icon: faAddressBook , color: '#551A8B'},
     { id: 3 , name: 'Facebook', link: '', icon: faFacebook , color: '#4267b2' },
     { id: 4 , name: 'Github', link: '', icon: faGithub , color: '#bd2c00' }, 
     { id: 5 , name: 'LinkedIn', link: '', icon: faLinkedin , color: '#0077b5' }
