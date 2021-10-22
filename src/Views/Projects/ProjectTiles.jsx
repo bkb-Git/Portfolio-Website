962139667de03a6a8782f57b5f71e9bae9261ebe
Billy K. Bett <82142770+bkb-Git@users.noreply.github.com>
2021-10-22 16:41:17 +0300

    added world bank graph application to portfolio

--- src/Views/Projects/ProjectTiles.jsx
+++ src/Views/Projects/ProjectTiles.jsx
@@ -1,17 +1,26 @@
 import Tile from './Tiles';
 import PortfolioWebsiteImg from '../../assets/PortfolioWebsiteImg-min.png';
+import GraphAppImg from '../../assets/WB-graph-app-cover.png';
 
 function ProjectTiles() {
   const exampleTiles = [
+    {
+      name: 'World Bank Graph App',
+      languages: ['React', 'Javascript', 'Bootstrap', 'HTML5', 'D3.js'],
+      description:
+        'Graphical representation of economic data using World Bank APIs',
+      backgroundImg: GraphAppImg,
+      link: 'https://github.com/bkb-Git/Graph-Application',
+      no: 1,
+    },
     {
       name: 'Portfolio Website',
       languages: ['React', 'Javascript', 'SCSS', 'HTML5'],
       description: 'This is my portfolio website',
       backgroundImg: PortfolioWebsiteImg,
       link: 'https://github.com/bkb-Git/Portfolio-Website',
-      no: 1,
+      no: 2,
     },
-    { no: 2 },
     { no: 3 },
     { no: 4 },
   ];
