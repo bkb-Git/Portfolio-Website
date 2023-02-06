7df15e44355d0161852dbdbf396582e30736e61f
Billy Bett <billybett@brian-mac.local>
2023-02-06 15:01:46 +0300

    fix: yet another fix

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import MainLayout from 'src/layout/MainLayout';
+import MainLayout from './layout/MainLayout';
 
-import Home from 'src/views/Home';
-import Projects from 'src/views/Projects';
-import Skills from 'src/views/Skills';
-import About from 'src/views/About';
-import ContactMe from 'src/views/ContactMe';
+import Home from './views/Home';
+import Projects from './views/Projects';
+import Skills from './views/Skills';
+import About from './views/About';
+import ContactMe from './views/ContactMe';
 
 import './styles/index.scss';
 
